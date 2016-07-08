44f8b4edfe555e099befc1fbe5cbdf349420554c
Anthony Maki <4cm4k1@gmail.com>
2016-07-08 16:53:59 -0500

    Complete basic and hard mode

--- client.js
+++ client.js
@@ -1,17 +1,31 @@
 angular.module('todoApp', []);
 
-angular.module('todoApp').controller('MainController', function($scope){
-  $scope.tasks = [];
+angular.module('todoApp').controller('MainController', function($scope) {
+    $scope.tasks = [];
 
-  $scope.addTask = function(newTask){
-    $scope.tasks.push({task: newTask, complete: false});
-    $scope.newTask = '';
-    console.log($scope.tasks);
-  }
+    $scope.addTask = function(newTask) {
+        if (newTask == '' || newTask == undefined) {
+            return;
+        } else {
+            $scope.tasks.push({
+                task: newTask,
+                complete: false
+            });
+            $scope.newTask = '';
+            console.log($scope.tasks);
+        }
+    }
 
-  $scope.completeStatus = function(){
+    $scope.removeTask = function(taskToBeRemoved) {
+      $scope.tasks.splice(taskToBeRemoved,1);
+    }
 
-  }
+    // function to remove multiple selected tasks from DOM
+    // and $scope.tasks
+    $scope.removeBulk(){
 
+    }
+
+    
 
 })
